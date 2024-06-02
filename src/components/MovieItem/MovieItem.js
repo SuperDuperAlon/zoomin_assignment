33af1f8e09de6f8c5c6372bae39824fa995cbabd
Alon Mlievski <alonmlievski@gmail.com>
2024-06-02 23:47:45 +0300

    updated movie item component

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,17 +1,56 @@
+import { useState, useRef } from 'react';
 import Image from '../Image/Image';
 import styles from './MovieItem.module.css'
+import data from '../../data/data.js';
 
 function MovieItem({ movie, onMovieSelect }) {
+  const [isOnHover, setIsOnHover] = useState(false); 
+  const [isCloserToLeft, setIsCloserToLeft] = useState(false);
+  const divRef = useRef(null);
+
+{/* When we hover over the item we want to show the trailer and to calculate it's position in the window so it won't overflow */}
+
+  function handleMouseEnter() {
+    if (isOnHover) return // We only want to do this once
+    if (divRef.current) {
+      const div = divRef.current;
+      window.innerWidth / 2 > div.offsetLeft ? setIsCloserToLeft(true) : setIsCloserToLeft(false)
+    }
+    setIsOnHover(true)
+  }
+
+  function handleMouseLeave() {
+    if (!isOnHover) return // We only want to do this once
+    setIsOnHover(false)
+  }
+
+  {/* Get the link to the trailer based on the episode ID */}
+
+  function getTrailer(id) {
+    if (!movie) return
+    const movieToUse = data.find(movie => movie.episode_id === id)
+    return movieToUse.linkToEmbed
+  }
+
   return (
-    <div className={styles.movie_item} onClick={() => onMovieSelect(movie)}
-      key={movie.episode_id}>
-      <Image movie={movie} className={styles.movie_item} />
-      <div className={styles.movie_item__content}>
-        <h2>{movie.title}</h2>
-        <button className={styles.movie_item__button} >
-          Watch
-        </button>
-      </div>
+    <div ref={divRef} className={styles.movie_item} onClick={() => onMovieSelect(movie)}
+      onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+      key={movie.episode_id} style={{ transformOrigin: isOnHover && isCloserToLeft ? 'left' : 'right' }}>
+      {isOnHover ?
+        <iframe autoPlay
+          frameBorder="0" src={`${getTrailer(movie.episode_id)}?autoplay=1&mute=1&origin=https://OurWebsiteDomain`}
+          allowFullScreen title={`${movie.title}`} height="278" data-ytbridge="vidSurrogate2" ></iframe>
+        :
+        <>
+          <Image movie={movie} className={styles.movie_item} />
+          <div className={styles.movie_item__content}>
+            <h2>{movie.title}</h2>
+            <button className={styles.movie_item__button} >
+              Watch
+            </button>
+          </div>
+        </>
+      }
     </div>
   );
 }
