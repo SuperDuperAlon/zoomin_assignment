bd75a9d7ba073ca71f4be88ff27ff4d5892dd193
Alon Mlievski <alonmlievski@gmail.com>
2024-06-02 23:47:12 +0300

    created more data for links

---
+++ src/data/data.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        name: 'A New Hope',
+        link: "https://www.youtube.com/watch?v=vZ734NWnAHA",
+        linkToEmbed: "https://www.youtube.com/embed/vZ734NWnAHA?si=KSoLdRDktFGumq-C",
+        episode_id: 4
+    }, {
+        name: 'The Empire Strikes Back',
+        link: "https://www.youtube.com/watch?v=JNwNXF9Y6kY",
+        linkToEmbed: "https://www.youtube.com/embed/JNwNXF9Y6kY?si=x_quJCWU4qwPCg68",
+        episode_id: 5
+    }, {
+        name: 'Return of the Jedi',
+        link: "https://www.youtube.com/watch?v=7L8p7_SLzvU",
+        linkToEmbed: 'https://www.youtube.com/embed/XgB4gaY2dWE?si=9yJKhnGyE25T_PJ0',
+        episode_id: 6
+    }, {
+        name: 'The Phantom Menace',
+        link: "https://www.youtube.com/watch?v=bD7bpG-zDJQ",
+        linkToEmbed: 'https://www.youtube.com/embed/bD7bpG-zDJQ?si=iQpH6Yc8OFbaZPfS',
+        episode_id: 1
+    }, {
+        name: 'Attack of the Clones',
+        link: "https://www.youtube.com/watch?v=gYbW1F_c9eM",
+        linkToEmbed: "https://www.youtube.com/embed/gYbW1F_c9eM?si=2Wckci8OM52m0dmQ",
+        episode_id: 2
+    }, {
+        name: 'Revenge of the Sith',
+        link: "https://www.youtube.com/watch?v=5UnjrG_N8hU",
+        linkToEmbed: "https://www.youtube.com/embed/5UnjrG_N8hU?si=stH926aMtkkYHo_i",
+        episode_id: 3
+    }
+]
+
+export default data;
